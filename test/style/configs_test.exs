adobe/elixir-styler

fiddle with private apis to ease iex playing

test/style/configs_test.exs
@@ -15,7 +15,7 @@ defmodule Styler.Style.ConfigsTest do
   alias Styler.Style.Configs
 
   test "only runs on exs files in config folders" do
-    {ast, _} = Styler.string_to_quoted_with_comments("import Config\n\nconfig :bar, boop: :baz")
+    {ast, _} = Styler.string_to_ast("import Config\n\nconfig :bar, boop: :baz")
     zipper = Styler.Zipper.zip(ast)
 
     for file <- ~w(dev.exs my_app.exs config.exs) do

test/style_test.exs
@@ -35,7 +35,7 @@ defmodule Styler.StyleTest do
   # After module
   """
 
-  @comments @code |> Styler.string_to_quoted_with_comments() |> elem(1)
+  @comments @code |> Styler.string_to_ast() |> elem(1)
 
   describe "displace_comments/2" do
     test "Doesn't lose any comments" do

test/support/style_case.ex
@@ -40,11 +40,11 @@ defmodule Styler.StyleCase do
       if styled != expected and ExUnit.configuration()[:trace] do
         IO.puts("\n======Given=============\n")
         IO.puts(before)
-        {before_ast, before_comments} = Styler.string_to_quoted_with_comments(before)
+        {before_ast, before_comments} = Styler.string_to_ast(before)
         dbg(before_ast)
         dbg(before_comments)
         IO.puts("======Expected AST==========\n")
-        {expected_ast, expected_comments} = Styler.string_to_quoted_with_comments(expected)
+        {expected_ast, expected_comments} = Styler.string_to_ast(expected)
         dbg(expected_ast)
         dbg(expected_comments)
         IO.puts("======Got AST===============\n")
@@ -107,7 +107,7 @@ defmodule Styler.StyleCase do
             dbg(styled_ast)
 
             IO.puts("expected:")
-            dbg(elem(Styler.string_to_quoted_with_comments(expected), 0))
+            dbg(elem(Styler.string_to_ast(expected), 0))
 
             IO.puts("code:\n#{styled}")
             flunk("")
@@ -133,11 +133,11 @@ defmodule Styler.StyleCase do
   end
 
   def style(code, filename \\ "testfile") do
-    {ast, comments} = Styler.string_to_quoted_with_comments(code)
+    {ast, comments} = Styler.string_to_ast(code)
     {styled_ast, comments} = Styler.style({ast, comments}, filename, on_error: :raise)
 
     try do
-      styled_code = styled_ast |> Styler.quoted_to_string(comments) |> String.trim_trailing("\n")
+      styled_code = styled_ast |> Styler.ast_to_string(comments) |> String.trim_trailing("\n")
       {styled_ast, styled_code, comments}
     rescue
       exception ->

emkguts/quokka

test/style/configs_test.exs
@@ -28,7 +28,7 @@ defmodule Quokka.Style.ConfigsTest do
   end
 
   test "only runs on exs files in config folders" do
-    {ast, _} = Quokka.string_to_quoted_with_comments("import Config\n\nconfig :bar, boop: :baz")
+    {ast, _} = Quokka.string_to_ast("import Config\n\nconfig :bar, boop: :baz")
     zipper = Quokka.Zipper.zip(ast)
 
     for file <- ~w(dev.exs my_app.exs config.exs) do

test/style_test.exs
@@ -35,7 +35,7 @@ defmodule Quokka.StyleTest do
   # After module
   """
 
-  @comments @code |> Quokka.string_to_quoted_with_comments() |> elem(1)
+  @comments @code |> Quokka.string_to_ast() |> elem(1)
 
   describe "displace_comments/2" do
     test "Doesn't lose any comments" do

test/support/style_case.ex
@@ -48,11 +48,11 @@ defmodule Quokka.StyleCase do
       if styled != expected and ExUnit.configuration()[:trace] do
         IO.puts("\n======Given=============\n")
         IO.puts(before)
-        {before_ast, before_comments} = Quokka.string_to_quoted_with_comments(before)
+        {before_ast, before_comments} = Quokka.string_to_ast(before)
         dbg(before_ast)
         dbg(before_comments)
         IO.puts("======Expected AST==========\n")
-        {expected_ast, expected_comments} = Quokka.string_to_quoted_with_comments(expected)
+        {expected_ast, expected_comments} = Quokka.string_to_ast(expected)
         dbg(expected_ast)
         dbg(expected_comments)
         IO.puts("======Got AST===============\n")
@@ -119,7 +119,7 @@ defmodule Quokka.StyleCase do
             dbg(styled_ast)
 
             IO.puts("expected:")
-            dbg(elem(Quokka.string_to_quoted_with_comments(expected), 0))
+            dbg(elem(Quokka.string_to_ast(expected), 0))
 
             IO.puts("code:\n#{styled}")
             flunk("")
@@ -149,11 +149,11 @@ defmodule Quokka.StyleCase do
   end
 
   def style(code, filename \\ "testfile") do
-    {ast, comments} = Quokka.string_to_quoted_with_comments(code)
+    {ast, comments} = Quokka.string_to_ast(code)
     {styled_ast, comments} = Quokka.style({ast, comments}, filename, on_error: :raise)
 
     try do
-      styled_code = styled_ast |> Quokka.quoted_to_string(comments) |> String.trim_trailing("\n")
+      styled_code = styled_ast |> Quokka.ast_to_string(comments) |> String.trim_trailing("\n")
       {styled_ast, styled_code, comments}
     rescue
       exception ->