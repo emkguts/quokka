4774504cb4738d69aab300a7dcdf642b6eaf4192
Ilya Bayaliyev <ilyabayaliyev@gmail.com>
2025-03-04 00:33:50 +0500

    feat: respect .formatter.exs line_length

--- test/config_test.exs
+++ test/config_test.exs
@@ -11,12 +11,12 @@ defmodule Quokka.ConfigTest do
   end
 
   test "respects the `:only` configuration" do
-    assert :ok = set!(only: [:deprecations])
+    assert :ok = set!(quokka: [only: [:deprecations]])
     assert [Deprecations] == Quokka.Config.get_styles()
   end
 
   test "respects the `:exclude` configuration" do
-    assert :ok = set!(exclude: [:deprecations])
+    assert :ok = set!(quokka: [exclude: [:deprecations]])
 
     # Check for one of the default configs
     assert Configs in Quokka.Config.get_styles()
@@ -26,13 +26,18 @@ defmodule Quokka.ConfigTest do
   end
 
   test "respects the `:only` and `:exclude` configuration" do
-    assert :ok = set!(only: [:configs, :deprecations], exclude: [:deprecations])
+    assert :ok = set!(quokka: [only: [:configs, :deprecations], exclude: [:deprecations]])
 
     assert [Configs] == Quokka.Config.get_styles()
   end
 
   test "only applies line-length changes if :line_length is present in the `:only` configuration" do
-    assert :ok = set!(only: [:line_length])
+    assert :ok = set!(quokka: [only: [:line_length]])
     assert [] == Quokka.Config.get_styles()
+  end
+
+  test "respects the formatter_opts line_length configuration" do
+    assert :ok = set!(line_length: 999)
+    assert Quokka.Config.get(:line_length) == 999
   end
 end
