b45d64b999f172c65e5c242e304c4e0c93eb40ef
Tyler Young <tyler@tylerayoung.com>
2026-02-12 08:54:02 -0600

    Fix crash when checking pipe start validity
    This fixes a crash when you started a pipe chain with a function call on a module that wasn't an atom literal (e.g., `@my_mod` instead of `MyApp.MyMod`). In the fix, rather than trying to be clever and interpolate the module attribute, I've just short-circuited the check against `pipe_chain_start_excluded_functions`.

--- lib/style/pipes.ex
+++ lib/style/pipes.ex
@@ -583,7 +583,8 @@ defmodule Quokka.Style.Pipes do
   defp valid_pipe_start?({{:., _, [{_, _, mod_list}, fun]}, _, arguments}) when is_list(mod_list) do
     not Quokka.Config.refactor_pipe_chain_starts?() or
       first_arg_excluded_type?(arguments) or
-      "#{Enum.join(mod_list, ".")}.#{fun}" in Quokka.Config.pipe_chain_start_excluded_functions()
+      (Enum.all?(mod_list, &is_atom/1) and
+         "#{Enum.join(mod_list, ".")}.#{fun}" in Quokka.Config.pipe_chain_start_excluded_functions())
   end
 
   # variable
